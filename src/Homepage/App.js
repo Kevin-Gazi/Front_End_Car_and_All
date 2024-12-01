c35a4d5ca1edbe87f509f294a07b2e9d40d80ce8
Yunus Kurt <23065141@student.hhs.nl>
2024-12-01 17:01:48 +0100

    Frontend Homepage-Home,Vehicles,Contact,Sign_up en foto toegevoegd

--- src/Homepage/App.js
+++ src/Homepage/App.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import './App.css';
-
+import logo from './CarAndAllFoto.png';
 
 function App() {
     return (
         <div className="Car_And_All">
-            <header className="Car_And_All-header">
-                <h1>Car AND All</h1>
-            </header>
             <main className="Car_And_All-main">
-                <nav class="navbar">
-                    <ul className="navbar_menu">
-                        <li className="menu-item">
-                            <a href="#" className="menu_links">Home</a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu_links">Vehicles</a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu_links">Contact</a>
-                        </li>
-                        <li className="menu-item">
-                            <a href="#" className="menu_links">Sign Up</a>
-                        </li>
-                    </ul>
+                <nav className="CarandAll">
+                    <div className="container">
+                        <h1 className="Title">Car And All</h1>
+                        
+                        {/* Logo/Image */}
+                        <div className="logo">
+                            <a href="/"> 
+                                <img src={logo} alt="Car and All Logo" />
+                            </a>
+                        </div>
+
+                        {/* Menu */}
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <a href="#" className="menu_links">Home</a>
+                            </li>
+                            <li className="menu-item">
+                                <a href="#" className="menu_links">Vehicles</a>
+                            </li>
+                            <li className="menu-item">
+                                <a href="#" className="menu_links">Contact</a>
+                            </li>
+                            <li className="menu-item">
+                                <a href="#" className="menu_links button">Sign Up</a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </main>
         </div>
+        
     );
 }
 
