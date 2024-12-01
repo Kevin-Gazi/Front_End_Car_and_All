00c90da1780c2a0cdcb84911bebc37ba0640f322
adamz <adam.zuurveld@gmail.com>
2024-12-01 17:41:41 +0100

    Favicon toegevoegd

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 
+function App() {
+    return (
+        <div className="Car_And_All">
+            <header className="Car_And_All-header">
+                <h1>Contact</h1>
+            </header>
+            <main className="Car_And_All-main">
+
+            </main>
+        </div>
+    );
+}
+
+export default App;
