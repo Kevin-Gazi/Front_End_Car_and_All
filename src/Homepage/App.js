f84570196602e86ea9e2662c6c572d826306e1e5
Kevin-Gazi <23067926@student.hhs.nl>
2024-12-01 17:23:12 +0100

    Added NavBar.js / bootstrap

--- src/Homepage/App.js
+++ src/Homepage/App.js
@@ -1,42 +1,16 @@
 import React from 'react';
 import './App.css';
 import logo from './CarAndAllFoto.png';
+import {NavBar} from "../NavBar/NavBar";
 
 function App() {
     return (
         <div className="Car_And_All">
             <main className="Car_And_All-main">
-                <nav className="CarandAll">
-                    <div className="container">
-                        <h1 className="Title">Car And All</h1>
-                        
-                        {/* Logo/Image */}
-                        <div className="logo">
-                            <a href="/"> 
-                                <img src={logo} alt="Car and All Logo" />
-                            </a>
-                        </div>
-
-                        {/* Menu */}
-                        <ul className="menu">
-                            <li className="menu-item">
-                                <a href="#" className="menu_links">Home</a>
-                            </li>
-                            <li className="menu-item">
-                                <a href="#" className="menu_links">Vehicles</a>
-                            </li>
-                            <li className="menu-item">
-                                <a href="#" className="menu_links">Contact</a>
-                            </li>
-                            <li className="menu-item">
-                                <a href="#" className="menu_links button">Sign Up</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+               <NavBar/>
             </main>
         </div>
-        
+
     );
 }
 
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../Homepage/CarAndAllFoto.png";
+
+export function NavBar() {
+    return <>
+        <nav className="CarandAll">
+            <div className="container">
+                <h1 className="Title">Car And All</h1>
+
+                {/* Logo/Image */}
+                <div className="logo">
+                    <a href="/">
+                        <img src={logo} alt="Car and All Logo"/>
+                    </a>
+                </div>
+
+                {/* Menu */}
+                <ul className="menu">
+                    <li className="menu-item">
+                        <a href="#" className="menu_links">Home</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#" className="menu_links">Vehicles</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#" className="menu_links">Contact</a>
+                    </li>
+                    <li className="menu-item">
+                        <a href="#" className="menu_links button">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </>
+}
