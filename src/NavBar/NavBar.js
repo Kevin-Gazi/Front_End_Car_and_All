d3c4c009dc21fdd9bd68fc6680514951bf12421f
Yunus Kurt <23065141@student.hhs.nl>
2024-12-02 13:19:05 +0100

    Homepage

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,37 +2,31 @@ import React from "react";
 import logo from "../Homepage/CarAndAllFoto.png";
 
 export function NavBar() {
-    return <>
+    return (
         <nav className="CarandAll">
             <div className="container">
-                <h1 className="Title">Car And All</h1>
+                <h1 className="Title"><a href = "/">Car And All</a></h1>
 
                 {/* Logo/Image */}
                 <div className="logo">
                     <a href="/">
-                        <img src={logo} alt="Car and All Logo"/>
+                        <img src={logo} alt="Car and All Logo" />
                     </a>
                 </div>
 
                 {/* Menu */}
                 <ul className="menu">
-
-                    <li className="menu-item">
+                    <li className="menu_item">
                         <a href="/" className="menu_links">Home</a>
                     </li>
-
-                    <li className="menu-item">
+                    <li className="menu_item">
                         <a href="/Contact/Contact" className="menu_links">Contact</a>
                     </li>
-
-                    <li className="menu-item">
+                    <li className="menu_item">
                         <a href="/vehicles" className="menu_links">Vehicles</a>
                     </li>
-
-                    {/* HIER KAN JE EEN NIEUWE REGEL TOEVOEGEN */}
-
                 </ul>
             </div>
         </nav>
-    </>
+    );
 }
