6c6cd06bc0a51d6d8d23c3c457a9a31d6ee927a6
Kevin-Gazi <23067926@student.hhs.nl>
2024-12-09 09:59:50 +0100

    center LoginScherm.js

--- src/Login/LoginScherm.js
+++ src/Login/LoginScherm.js
@@ -1,31 +1,34 @@
 import React from "react";
 import './LoginScherm.css';
 import { FaUserAlt, FaLock } from "react-icons/fa";
+
 function LoginScherm() {
     return (
-        <div className="wrapper">
-            <form action="">
-                <h1>Login</h1>
-                <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
-                    <FaUserAlt className="icon" />
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
-                    <FaLock className="icon" />
-                </div>
-                
-                <div className="remember-forgot">
-                    <a href = "#">Forgot password?</a>
-                </div>
-                <button type = "submit">Login</button>
-                
-                <div className="register-link">
-                    <p>Don't have an account? <a href = "">Register</a></p>
-                </div>
-            </form>
+        <div className="login-container">
+            <div className="wrapper">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="Username" required />
+                        <FaUserAlt className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required />
+                        <FaLock className="icon" />
+                    </div>
+
+                    <div className="remember-forgot">
+                        <a href="#">Forgot password?</a>
+                    </div>
+                    <button type="submit">Login</button>
+
+                    <div className="register-link">
+                        <p>Don't have an account? <a href="#">Register</a></p>
+                    </div>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
 
 export default LoginScherm;
